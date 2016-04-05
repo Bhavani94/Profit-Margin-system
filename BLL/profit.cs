cebaf22ef84c811b1ea946f02748b997471d728e
Bhavani Sundara Raman <bhavanimay94@gmail.com>
2016-04-05 07:00:34 -0400

    Added files via upload

---
+++ BLL/profit.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using DAL;
+using EF;
+
+namespace BLL
+{
+    public class profit
+    {
+        DAL.profit obj = new DAL.profit();
+        public void addExpenses(int ProjectID, int AssociateID, string ExpenseType, string ExpenseDate, int Amount, string Currency, string Descr)
+        {
+            obj.addExpenses(ProjectID, AssociateID, ExpenseType, ExpenseDate, Amount, Currency, Descr);
+        }
+        public IEnumerable<usp_displayExpenses_Result> dispExp(int ProjectID)
+        {
+            return obj.dispExp(ProjectID);
+        }
+        public IEnumerable<usp_displayTotalExpense_Result> dispTExp(int ProjectID)
+        {
+            return obj.dispTExp(ProjectID);
+        }
+        public IEnumerable<usp_viewTimesheet_Result> viewTimesheet(int ProjectID)
+        {
+            return obj.viewTimesheet(ProjectID);
+        }
+        public void addTime(int TimesheetID, int ProjectID, string Activity, string Datee, double hourss)
+        {
+            obj.addTime(TimesheetID, ProjectID, Activity, Datee, hourss);
+        }
+        public IEnumerable<usp_viewProject_Result> viewProject(int ProjectID)
+        {
+            return obj.viewProject(ProjectID);
+        }
+    }
+}
